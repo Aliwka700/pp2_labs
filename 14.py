7bd0ded57eee4a55f508768dc3d61bbb33675b87
Aliwka700 <ali_akhmet@kbtu.kz>
2025-02-12 16:47:42 +0500

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,16 @@
+def import_functions():
+    print(grams_to_ounces(100))
+    print(fahrenheit_to_celsius(100))
+    print(solve(35, 94))
+    print(filter_prime([10, 11, 12, 13, 14, 15, 16, 17]))
+    print(string_permutations("abc"))
+    print(reverse_sentence("We are ready"))
+    print(has_33([1, 3, 3]))
+    print(spy_game([1, 2, 4, 0, 0, 7, 5]))
+    print(sphere_volume(3))
+    print(unique_list([1, 2, 2, 3, 4, 4, 5]))
+    print(is_palindrome("madam"))
+    histogram([4, 9, 7])
+
+if __name__ == "__main__":
+    import_functions()
