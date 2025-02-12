a0984a544293ccf02ed2a8a940dbd6ec02670972
Aliwka700 <ali_akhmet@kbtu.kz>
2025-02-12 16:45:52 +0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def filter_prime(numbers):
+    return [num for num in numbers if is_prime(num)]
